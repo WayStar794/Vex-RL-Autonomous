760b0c5eb5fcc55b27dc0e484202844eb2559028
Michael <crj.for.mlj@gmail.com>
2025-07-26 18:05:35 -0500

    Started work on serial transmission

--- PROS/robot auton scripts/src/main.cpp
+++ PROS/robot auton scripts/src/main.cpp
@@ -1,5 +1,9 @@
 #include "main.h"
-
+#include <iostream>     // For std::cout, std::cin, std::cerr
+#include <string>       // For std::string
+#include <vector>       // For std::vector
+#include <sstream>      // For std::istringstream (used in split function)
+#include <map>          // For std::map (used to store commands)
 /**
  * A callback function for LLEMU's center button.
  *
@@ -73,22 +77,28 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+
 void opcontrol() {
+	
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
-
+	pros::Motor FL({1});
+	pros::Motor FR({-10});
+	pros::Motor RL({11});
+	pros::Motor RR({-21});
+	
+	
 	while (true) {
+		if (master.get_digital(DIGITAL_A)) {
+			FL.set_zero_position(0);
+			FR.set_zero_position(0);
+			RL.set_zero_position(0);
+			RR.set_zero_position(0);
+		}
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		pros::delay(10);                               // Run for 20 ms then update
 	}
 }
